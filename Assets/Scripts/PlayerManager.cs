50d2de028f3427780f2bf64c140ee61d3a6ca1b2
thrilliams <sorenw@khanlabschool.org>
2019-06-18 13:42:18 -0700

    Fix player collider sizing

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,9 +7,8 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         Vector3 size = GetComponent<SpriteRenderer>().bounds.size;
-        GetComponent<BoxCollider>().center = new Vector3(0.05f, 0.05f, 0.05f);
-        GetComponent<BoxCollider>().size = size + new Vector3(0.2f, 0.2f, 0.2f);
-        float scale = 1 / Mathf.Max(size.x, size.y, size.z);
-        transform.localScale = new Vector3(scale, scale, scale);
+        float max = Mathf.Max(size.x, size.y, size.z);
+        GetComponent<BoxCollider>().size = new Vector3(max, max, max);
+        transform.localScale = new Vector3(1 / max, 1 / max, 1 / max);
     }
 }
